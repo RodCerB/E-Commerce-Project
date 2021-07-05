108e576015c7982889509d5db1ae6d2cf2897c04
rodcerb <rodrigo.cbraga@hotmail.com>
2021-07-05 16:35:46 -0300

    Estruturado o fetch dos produtos, preparado no reducer para sucesso e para falha

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,7 +15,12 @@ import {
 } from '../actions'
 
 const initialState = {
-  isSidebarOpen:false
+  isSidebarOpen:false,
+  products_loading:false,
+  products_error:false,
+  products:[],
+  featured_products:[],
+
 }
 
 const ProductsContext = React.createContext()
@@ -30,6 +35,22 @@ export const ProductsProvider = ({ children }) => {
   const closeSidebar = () =>{
     dispatch({type:SIDEBAR_CLOSE})
   }
+
+  // pegando a api dos produtos
+  const fetchProducts = async(url) =>{
+    dispatch({type:GET_SINGLE_PRODUCT_BEGIN})
+    try{
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({type:GET_PRODUCTS_SUCCESS, payload:products})
+    } catch(error) {
+
+    }
+  }
+
+  useEffect(()=>{
+    fetchProducts(url)
+  },[])
 
   return (
     <ProductsContext.Provider value={{...state, openSidebar, closeSidebar}}>
