90d25674f84f3b145c50f309b482535d8a2e966d
rodcerb <rodrigo.cbraga@hotmail.com>
2021-07-06 18:26:35 -0300

    preparado paginas de erro e loading para os single products

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,6 +15,36 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
+  // Para pegarmos a id do produto individual, usamos o Params que é do próprio react-router, se lembra que o link individual dos produtos leva o id deles? Pois bem, agora usando o params re-pegamos esse id que vamos usar para chamar a api
+  const {id} = useParams()
+  const history = useHistory()
+  const {
+    single_product_loading:loading, 
+    single_product_error:error, 
+    single_product:product, 
+    fetchSingleProduct} = useProductsContext()
+
+  useEffect(()=>{
+    fetchSingleProduct(`${url}${id}`)
+  },[])
+
+  useEffect(()=>{
+    console.log(error)
+    if(error){
+      // aqui vamos deixar programado para caso de erro na chamada do singlebook, depois de 3 segundo irá retornar para a home
+      setTimeout(()=>{
+        history.push('/')
+      },3000)
+    }
+    // precisamos colocar para o useEffect mudar com o false pq se lembra, ao chamar a api colocamos para deixar o false sempre falso, e só depois ele vai para true em caso de erro. 
+  },[false])
+
+  if(loading){
+    return <Loading />
+  }
+  if(error){
+    return <Error />
+  }
   return <h4>single product page</h4>
 }
 
