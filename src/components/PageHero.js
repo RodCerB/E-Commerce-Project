e85630ca029bcec6689e68919131ffbee2ce4faa
rodcerb <rodrigo.cbraga@hotmail.com>
2021-07-07 16:51:22 -0300

    estrutura do single product page feita

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -2,11 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const PageHero = ({title}) => {
+// Nas páginas dos produtos individuais, nao ficará o page hero apenas home/title, deve ter o link para products também, por isso iremos colocar mais uma informação chegando no pagehero além do title.
+
+const PageHero = ({title, product}) => {
+
   return <Wrapper>
     <div className="section-center">
       <h3>
-        <Link to='/'>Home</Link>/ {title}
+        <Link to='/'>Home</Link> 
+        {product && <Link to='/products'>/ Products</Link>}
+        / {title}
       </h3>
     </div>
   </Wrapper>
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -29,7 +29,6 @@ const SingleProductPage = () => {
   },[])
 
   useEffect(()=>{
-    console.log(error)
     if(error){
       // aqui vamos deixar programado para caso de erro na chamada do singlebook, depois de 3 segundo irá retornar para a home
       setTimeout(()=>{
@@ -45,7 +44,41 @@ const SingleProductPage = () => {
   if(error){
     return <Error />
   }
-  return <h4>single product page</h4>
+  
+  const {name, price, description, stock, stars, reviews, id:sku, company, images} = product
+
+  return <Wrapper>
+    {/* Como falamos na pagina do pagehero, aqui no singleproduct precisamos passar a informação producst tbm para add o link para referida pagina */}
+    <PageHero title={name} product/>
+    <div className="section section-center page">
+      <Link to='/products' className='btn'>
+        back to products
+      </Link>
+      <div className="products-center">
+        <ProductImages />
+        <section className="content">
+          <h2>{name}</h2>
+          <Stars />
+          <h5 className='price'>{formatPrice(price)}</h5>
+          <p className='desc'>{description}</p>
+          <p className='info'>
+            <span>Available : </span>
+            {stock > 0 ? 'In stock' : 'out of stock'}
+          </p>
+          <p className='info'>
+            <span>SKU : </span>
+            {sku}
+          </p>
+          <p className='info'>
+            <span>Brand : </span>
+            {company}
+          </p>
+          <hr />
+          {stock > 0 && <AddToCart />}
+        </section>
+      </div>
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.main`
